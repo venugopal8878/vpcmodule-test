feb938d4e7df298aa2f0c64314716479fb8c7062
venugopal <mallavallivenu@gmail.com>
2024-10-12 13:57:29 +0530

    vpc module

--- output.tf
+++ output.tf
@@ -5,3 +5,11 @@ output "vpc"{
 # output "az_info"{
 #     value = module.vpc.az_info
 # }
+
+# output "default_vpc_info"{
+#     value = module.vpc.default_vpc_info
+# }
+
+# output "main_route_table_info"{
+#     value = module.vpc.main_route_table_info
+# }
