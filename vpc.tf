062b8193a308b07a9cdbcd0c65604b2e4006e42a
venugopal <mallavallivenu@gmail.com>
2024-10-19 08:58:09 +0530

    source

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,6 @@
 module "vpc"{
-    source = "../terraform-aws-vpc"
+    #source = "../terraform-aws-vpc"
+    source = "git::https://github.com/venugopal8878/terraform-aws-vpc?ref=main"
     project_name = var.project_name
     environment = var.environment
     common_tags = var.common_tags
